1f53cd24203e350ccb3d917cb49eb4d51deb21cf
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-23 00:32:59 +0300

    Create theme.js

---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    text: '#212121',
+    textAccent: '#ffffff',
+    background: '#f4f4f4',
+    btn: '#5f5f5f',
+  },
+  spacing: value => `${4 * value}px`,
+};
