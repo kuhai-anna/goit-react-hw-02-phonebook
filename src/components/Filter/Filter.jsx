4dae3b737d2026f0c7b1ccf32bb4eca70fbe9767
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-24 03:47:02 +0300

    Create Filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,15 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-export const Filter = () => {};
+export const Filter = ({ value, onChangeFilter }) => {
+  return (
+    <label>
+      <span>Find contacts by name</span>{' '}
+      <input type="text" value={value} onChange={onChangeFilter}></input>
+    </label>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
