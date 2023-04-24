d73297f1298ca9666594498df52f83eba7134d19
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-25 00:01:53 +0300

    Add colors to the theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -2,8 +2,12 @@ export const theme = {
   colors: {
     text: '#212121',
     textAccent: '#ffffff',
-    background: '#f4f4f4',
-    btn: '#5f5f5f',
+    bgd: '#f4f4f4',
+    bgdAccent: '#d2d2d2',
+    border: '#d2d2d2',
+    borderHover: '#a8a8a8',
+    btn: '#a8a8a8',
+    btnHover: '#d2d2d2',
   },
   spacing: value => `${4 * value}px`,
 };
